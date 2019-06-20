779e4edccd727bc5b8b6c14229df70be5075efc3
Jose Lins <joselinsneto182@gmail.com>
2019-06-20 13:05:47 -0300

    ajustando e testando a delecao de cliente

--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -60,7 +60,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir porque há objetos relacionados.");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados.");
 		}
 	}
 	
