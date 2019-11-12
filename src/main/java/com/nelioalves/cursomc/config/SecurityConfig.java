37b9c10e0d3fa747928e08f9570cdcb00d398aeb
Jose Lins <joselinsneto182@gmail.com>
2019-11-12 08:04:42 -0300

    Implementando autorizacao

--- src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.nelioalves.cursomc.security.JWTAuthenticationFilter;
+import com.nelioalves.cursomc.security.JWTAuthorizationFilter;
 import com.nelioalves.cursomc.security.JwtUtil;
 
 @Configuration
@@ -67,6 +68,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			//Para qualquer outra requisição que não foi explicitamente permitida, exigir autenticação
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
