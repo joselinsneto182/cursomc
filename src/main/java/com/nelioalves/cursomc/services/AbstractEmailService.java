91453613a0d5dc11759518a0ebf0c81da3b13d00
joselins <joselinsneto182@gmail.com>
2019-11-15 10:59:40 -0300

    Esqueci a senha

--- src/main/java/com/nelioalves/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/AbstractEmailService.java
@@ -2,17 +2,13 @@ package com.nelioalves.cursomc.services;
 
 import java.util.Date;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService{
@@ -23,8 +19,8 @@ public abstract class AbstractEmailService implements EmailService{
 	@Autowired
 	private TemplateEngine templateEngine;
 	
-	@Autowired
-	private JavaMailSender javaMailSender;
+//	@Autowired
+//	private JavaMailSender javaMailSender;
 	
 	@Override
 	public void sendOrderConfirmationEmail(Pedido pedido) {
@@ -48,25 +44,41 @@ public abstract class AbstractEmailService implements EmailService{
 		return templateEngine.process("email/confirmacaoPedido", context);
 	}
 	
-	protected MimeMessage prepareMimeMessageFromPedido(Pedido pedido) throws MessagingException {
-		MimeMessage mm = javaMailSender.createMimeMessage();
-		MimeMessageHelper mmh = new MimeMessageHelper(mm, true);
-		mmh.setTo(pedido.getCliente().getEmail());
-		mmh.setFrom(sender);
-		mmh.setSubject("Pedido confirmado! Código: "+pedido.getId());
-		mmh.setText(htmlFromTemplatePedido(pedido),true);
-		mmh.setSentDate(new Date(System.currentTimeMillis()));
-		return mm;
-	}
+//	protected MimeMessage prepareMimeMessageFromPedido(Pedido pedido) throws MessagingException {
+//		MimeMessage mm = javaMailSender.createMimeMessage();
+//		MimeMessageHelper mmh = new MimeMessageHelper(mm, true);
+//		mmh.setTo(pedido.getCliente().getEmail());
+//		mmh.setFrom(sender);
+//		mmh.setSubject("Pedido confirmado! Código: "+pedido.getId());
+//		mmh.setText(htmlFromTemplatePedido(pedido),true);
+//		mmh.setSentDate(new Date(System.currentTimeMillis()));
+//		return mm;
+//	}
+	
+//	@Override
+//	public void sendOrderConfirmationHtmlEmail(Pedido pedido) {
+//		try {
+//			MimeMessage mm = prepareMimeMessageFromPedido(pedido);
+//			sendHtmlEmail(mm);
+//		}catch(MessagingException e) {
+//			sendOrderConfirmationEmail(pedido);
+//		}
+//	}
 	
 	@Override
-	public void sendOrderConfirmationHtmlEmail(Pedido pedido) {
-		try {
-			MimeMessage mm = prepareMimeMessageFromPedido(pedido);
-			sendHtmlEmail(mm);
-		}catch(MessagingException e) {
-			sendOrderConfirmationEmail(pedido);
-		}
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage msg = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(msg);
+	}
+
+	private SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage msg = new SimpleMailMessage();
+		msg.setTo(cliente.getEmail());
+		msg.setFrom(sender);
+		msg.setSubject("Solicitação de nova senha");
+		msg.setSentDate(new Date(System.currentTimeMillis()));
+		msg.setText("Nova senha: "+newPass);
+		return msg;
 	}
 	
 }
--- src/main/java/com/nelioalves/cursomc/services/EmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/EmailService.java
@@ -1,9 +1,8 @@
 package com.nelioalves.cursomc.services;
 
-import javax.mail.internet.MimeMessage;
-
 import org.springframework.mail.SimpleMailMessage;
 
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -12,7 +11,9 @@ public interface EmailService {
 	
 	void sendEmail(SimpleMailMessage msg);
 	
-	void sendOrderConfirmationHtmlEmail(Pedido obj);
+//	void sendOrderConfirmationHtmlEmail(Pedido obj);
+//	
+//	void sendHtmlEmail(MimeMessage msg);
 	
-	void sendHtmlEmail(MimeMessage msg);
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
